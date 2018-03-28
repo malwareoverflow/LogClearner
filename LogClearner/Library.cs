8eac5ef1d5c747ef88ad9e5d42405efdc83111b5
Taha Temuri <tahatemuri7@outlook.com>
2018-03-28 14:42:29 +0500

    Code

---
+++ LogClearner/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogClearner
+{
+  public static  class Library
+    {
+        public static void writetoLog(Exception ex)
+        {
+         
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Logfile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ":" + ex.ToString().Trim() + ";" + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception)
+            {
+
+               
+            }
+        }
+    }
+}
---
+++ LogClearner/Service1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogClearner
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+       
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                foreach (var eventLog in EventLog.GetEventLogs())
+                {
+                    eventLog.Clear();
+                    eventLog.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Library.writetoLog(ex);
+            }
+        }
+
+        protected override void OnStop()
+        {
+        
+        }
+    }
+}
